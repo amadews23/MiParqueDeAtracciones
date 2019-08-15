55d54b0404de3018c34a840b6aeaade13c5f5067
amadews23 <tolo@tovilo.es>
2019-08-15 10:48:47 +0200

    We rename the Class

---
+++ src/Gestion/LineaInformeVisitantes.java
@@ -0,0 +1,58 @@
+package Gestion;
+
+/**
+ *
+ * @author Bartolome Vich Lozano
+ * @version 1.0 2019/8/7
+ */
+
+public class LineaInformeVisitantes {
+    
+    private final String fecha;
+    private int numero;
+    private float precio;
+    
+    public LineaInformeVisitantes( String fecha) {
+        this.fecha = fecha;
+        this.numero = 0;
+        this.precio = 0;
+    }
+    public LineaInformeVisitantes( String fecha, float precio) {
+        this.fecha = fecha;
+        this.numero = 0;
+        this.precio = precio;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public String getFecha() {
+        return fecha;
+    }
+
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public float getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(float precio) {
+        this.precio = precio;
+    }
+
+    public void sumar() {
+        this.numero++;
+    }
+    
+    public void sumarNumero(int numero) {
+        this.numero = this.numero + numero;
+    }
+    
+    public void sumarPrecio(float precio) {
+        this.precio = this.precio + precio;
+    }
+}
